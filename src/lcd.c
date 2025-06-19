d705ed99667a8cb5496f6dbf44d0477c90e7a9cc
kamehiro4771 <kamehiro4771@outlook.jp>
2025-06-19 21:54:44 +0900

    LCDディスプレイのプログラム作り始めました！ メニュー画面を表示したい

--- src/lcd.c
+++ src/lcd.c
@@ -12,7 +12,7 @@
 /*
  * マクロ定義
  */
-#define FUNCTION_SET (0x02c)//インターフェースデータ長8ビットデューティーの設定1/16に設定
+#define FUNCTION_SET (0x03c)//インターフェースデータ長8ビット,デューティーの設定1/16に設定
 #define CLEAR_DISPLAY (0x001)
 #define RETURN_HOME (0x002)
 #define ENTRY_MODE_RIGHT (0x006)//表示を右にシフトに設定する。表示全体はシフトしない
@@ -25,18 +25,50 @@
 #define BUSYFLAG_LEADOUT (0x100)//内部操作中かどうか確認する
 #define WRITE_DATA (0x200)//DDRAMに書き込む
 #define READ_DATA (0x300)//DDRAMを読む
+#define BUSY_FLAG (PORT4.PORT.BIT.B7)
+#define DB (PORT4.DDR.BYTE)
+#define RS (PORTA.DDR.BIT.B2)
+#define RW (PORTA.DDR.BIT.B1)
+#define E (PORTA.DDR.BIT.B3)
 
+ /*********************************************************/
+ /*インストラクションコードを送る	　　　				  */
+ /*void instruction_sending(unsigned short instruction) 　*/
+ /*引数：unsigned short instruction						  */
+ /*********************************************************/
+void instruction_sending(unsigned short instruction)
+{
+	DB = (instruction & 0x00ff);
+	RW = (instruction >> 8) & 1
+		RS |= (instruction >> 8) & 2;
+	E = 1;
+	cmt2_wait(2, CKS8);//約300ns待機
+	E = 0;
+}
+
+/*********************************************************/
+/*LCDディスプレイ初期化関数								 */
+/*void lcd_init(void)									 */
+/*********************************************************/
 void lcd_init(void)
 {
 	PORT4.DDR.BYTE	= 0xff;//ポート4を出力ポートに設定
 	PORTA.DDR.BYTE	= 0x0d;//PA1,PA2，PA3を出力ポートに設定
+	instruction_sending(FUNCTION_SET);//ファンクションセット1回目
 	cmt2_wait(60000,CKS32);//40ms待機
-	PORT4.DR.BYTE	= FUNCTION_SET;
-	cmt2_wait(56,CKS32);//37?待機
-	PORT4.DR.BYTE	= DISPLAY_ON;
-	cmt2_wait(56,CKS32);//37?待機
-	PORT4.DR.BYTE	= CLEAR_DISPLAY;
+	instruction_sending(FUNCTION_SET);//ファンクションセット2回目
+	cmt2_wait(150, CKS32);//100μs待機
+	instruction_sending(FUNCTION_SET);//ファンクションセット3回目
+	while (BUSY_FLAG == 0) {
+	}
+	instruction_sending(FUNCTION_SET);//ファンクションセット4回目
+	while (BUSY_FLAG == 0) {
+	}
+	instruction_sending(DISPLAY_OFF);
+	while (BUSY_FLAG == 0) {
+	}
+	instruction_sending(CLEAR_DISPLAY);
 	cmt2_wait(2280,CKS32);//1.52ms待機
-	PORT4.DR.BYTE	= ENTRY_MODE_RIGHT;
+	instruction_sending(ENTRY_MODE_RIGHT);
 }
 
